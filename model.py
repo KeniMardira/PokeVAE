f69fbce6d8192ffad090e45e768988e0b2c8d431
keniMawson <keni@mawson.io>
2018-05-04 06:25:50 +0000

    modularised files

---
+++ model.py
@@ -0,0 +1,79 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+
+LATENT_DIM = 40
+RESIZE = 128
+
+class Net(nn.Module):
+    def __init__(self, latent_dim=LATENT_DIM):
+        super(Net, self).__init__()
+        
+        #encoder
+        self.conv_e1 = nn.Conv2d(3, 32, kernel_size=4, stride=2, padding=1)
+        self.bn_e1 = nn.BatchNorm2d(32)
+        self.conv_e2 = nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1)
+        self.bn_e2 = nn.BatchNorm2d(64)
+        self.conv_e3 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.bn_e3 = nn.BatchNorm2d(128)
+        self.conv_e4 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
+        self.bn_e4 = nn.BatchNorm2d(256)
+        
+        self.fc_mean = nn.Linear(256*RESIZE//16*RESIZE//16, latent_dim)
+        self.fc_logvar = nn.Linear(256*RESIZE//16*RESIZE//16, latent_dim)
+        
+        #decoder
+        self.fc_d = nn.Linear(latent_dim, 256*RESIZE//16*RESIZE//16)
+        
+        self.up = nn.Upsample(scale_factor=2)
+        
+        self.conv_d1 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.bn_d1 = nn.BatchNorm2d(128)
+        self.conv_d2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn_d2 = nn.BatchNorm2d(64)
+        self.conv_d3 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.bn_d3 = nn.BatchNorm2d(32)
+        self.conv_d4 = nn.Conv2d(32, 3, kernel_size=3, stride=1, padding=1)
+        
+    def encoder(self, x):
+        ''' encoder: q(z|x)
+            input: x, output: mean, logvar
+        '''
+        x = F.leaky_relu(self.bn_e1(self.conv_e1(x)))
+        x = F.leaky_relu(self.bn_e2(self.conv_e2(x)))
+        x = F.leaky_relu(self.bn_e3(self.conv_e3(x)))
+        x = F.leaky_relu(self.bn_e4(self.conv_e4(x)))
+        x = x.view(-1, x.shape[1] * x.shape[2] * x.shape[3])
+        z_mean = self.fc_mean(x)
+        z_logvar = self.fc_logvar(x)
+        return z_mean, z_logvar
+    
+    def latent(self, z_mu, z_logvar):
+        ''' 
+            encoder: z = mu + sd * e
+            input: mean, logvar. output: z
+        '''
+        sd = torch.exp(z_logvar * 0.5)
+        e = Variable(torch.randn(sd.size())).cuda()
+        z = e.mul(sd).add_(z_mu)
+        return z 
+    
+    def decoder(self, z):
+        '''
+            decoder: p(x|z)
+            input: z. output: x
+        '''
+        x = self.fc_d(z)
+        x = x.view(-1, 256, RESIZE//16, RESIZE//16)
+        x = F.leaky_relu(self.bn_d1(self.conv_d1(self.up(x))))
+        x = F.leaky_relu(self.bn_d2(self.conv_d2(self.up(x))))
+        x = F.leaky_relu(self.bn_d3(self.conv_d3(self.up(x))))
+        x = F.sigmoid(self.conv_d4(self.up(x)))
+        return x.view(-1,3,RESIZE,RESIZE)
+
+    def forward(self, x):
+        z_mean, z_logvar = self.encoder(x)
+        z = self.latent(z_mean, z_logvar)
+        x_out = self.decoder(z)
+        return x_out, z_mean, z_logvar
---
+++ utils.py
@@ -0,0 +1,82 @@
+import model
+from model import *
+
+import torch.utils.data as Utils
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+from tqdm import tnrange
+import scipy.ndimage
+import cv2
+import os
+import csv
+import pandas as pd
+from IPython.display import clear_output
+import imageio
+
+import torch.optim as optim
+#Define Criterion
+
+
+def create_csv():
+    pokelist = os.listdir("./Pokemon/")
+    with open("./PokeList", 'w') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(pokelist)
+        
+        
+# Dataloader class that works with VCTK datasets. Init require the path to a folder where voice_data are stored.
+class PokeSet(Utils.Dataset): # Main dataset class for dataloader
+    def __init__(self, path): # takes two input, the input and output Tensors
+        self.path = path
+        self.pokelist = pd.read_csv("PokeList",header=None)
+
+    def __len__(self): # must be written for dataset module
+        return len(os.listdir(self.path))
+    
+    def __getitem__(self,index): # must be written for dataset module
+        pokemon = mpimg.imread(self.path + self.pokelist[index][0])
+        pokemon = cv2.resize(pokemon, (RESIZE,RESIZE))
+        pokemon = (pokemon - np.min(pokemon))/np.max(pokemon - np.min(pokemon))
+        return pokemon
+
+    
+def criterion(x_out, target, z_mean, z_logvar, alpha = 1, beta = 10):
+    bce = F.mse_loss(x_out, target, size_average=False) #Use MSE loss for images
+    kl = -0.5 * torch.sum(1 + z_logvar - (z_mean**2) - torch.exp(z_logvar)) #Analytical KL Divergence - Assumes p(z) is Gaussian Distribution
+    loss = ((alpha * bce) + (beta * kl)) / x_out.size(0)    
+    return loss, bce, kl
+
+
+def load_checkpoint(filename):
+    '''Loading function for the model before and during training
+    From a checkpoint file, it loads and returns all necessary data (mode, optimiser, epoch number, losses)
+    input: filename -> The name of the checkpoint file to be opened (.pth or .pt)
+    output: net -> The saved model, including weights and biases
+    output: epoch -> The epoch number at which the training was saved
+    output: loss_save -> An array of all the saved batch losses during training
+    output: optimizer -> The current state of the optimiser with its updated learning rates from training'''
+    
+    net = Net().cuda() # Initialize model  
+    checkpoint = torch.load(filename) # load checkpoint data
+    net.load_state_dict(checkpoint['state_dict'])
+    epoch = checkpoint['epoch']
+    losses = checkpoint['losses']
+    optimizer = optim.Adam(net.parameters(), lr=0.0001)
+    optimizer.load_state_dict(checkpoint['optimizer'])
+    print("Loaded checkpoint: " + filename)
+    return net, epoch, losses, optimizer#, scheduler
+
+
+def pokePlot(images, vae, ROW, COL):
+    f, axarr = plt.subplots(ROW,COL, figsize=(25,ROW*4))
+    for row in range(ROW//2):
+        for col in range(COL):
+            image = images[col+(COL*row),:,:,:].unsqueeze(0)
+            axarr[2*row,col].imshow(image.squeeze().numpy())
+            image = image.permute(0,3,1,2)
+            x_out, z_mean, z_logvar = vae(Variable(image.float()).cuda())
+            x_out = x_out.permute(0,2,3,1)
+            axarr[2*row+1,col].imshow(x_out.data.cpu().squeeze().numpy())
+    plt.show()
